2e9174911ca4206bf061517970a7eddf104ea97a
Dhruv-Rana1507 <ranadhruv019@gmail.com>
2024-01-03 20:55:49 +0530

    made the slug proper and connect the blogs with their apropiate data with heading and add proper responsive css

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React ,{useEffect,useState}from 'react'
 import { useRouter } from 'next/router'
 import styles from '@/styles/Blogpost.module.css'
 const slug = () => {
-    const router = useRouter();
-    const {slug} = router.query;
+    const [blog, setBlog] = useState()
+  const router = useRouter();
+  useEffect(() => {
+    if(!router.isReady) return 
+          const {slug} = router.query;
+          fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then((a)=>{
+            return a.json();})
+            .then(parsed =>{
+             setBlog(parsed)
+             console.log("its coming")
+           })
+        }, [router.isReady])
+        
+      
     return (
       <>
       <main className={`${styles.main} }`}>
-      <h1>Title of the Post</h1>
-      <hr / >
-    <div>This is the Post for the {slug}</div>
+      <h1 className={styles.title}>{blog && blog.title}</h1>
+      <hr style={{ border: '3px dotted #ddd' }} />
+
+    <div className={styles.content}>{blog && blog.content}{slug}
+    <br/>
+    <br/>
+    <hr style={{ border: '3px dotted #ddd' }} />
+
+    < h2 className={styles.author}>Author : = {blog && blog.author}</h2>
+    </div>
    </main>
     </>
   )
